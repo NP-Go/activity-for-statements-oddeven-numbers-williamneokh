4ee17e76dc0386e48e32d5e2b6ee8df89475e715
williamneo <williamneokh@gmail.com>
2022-03-30 00:00:23 +0800

    done odd or even number continue asking for input

--- oddEvenDecision.go
+++ oddEvenDecision.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"log"
+)
+
 func main() {
-	//Insert code here
+	//ask user for number
+	var number int
+	for {
+		fmt.Println("Please key in a number")
+		_, _ = fmt.Scanln(&number)
+
+		checkEvenOrOdd(number)
+		fmt.Println("------------")
+
+	}
+}
+
+func checkEvenOrOdd(number int) {
+
+	if number%2 == 0 {
+		fmt.Println(number, "Is an even number")
+	} else if number%2 == 1 {
+		fmt.Println(number, "Is an odd number")
+	} else {
+		log.Fatal(number%2, "unknown")
+	}
 }
